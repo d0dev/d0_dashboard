8b04b38f2b086467aea3bdbb34f1eb0cbd9eddf2
d0dev <imaluofficial@gmail.com>
2024-02-17 13:53:47 +0100

    Update Angular dependencies and modify side menu component

--- frontend/src/app/components/side-menu/side-menu.component.ts
+++ frontend/src/app/components/side-menu/side-menu.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list'; // Add this import
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { TitleCasePipe, CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+//import { DashboardComponent } from '../../../dashboard/component/dashboard/dashboard.component';
+
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [MatIconModule, MatListModule, RouterModule], // Add MatListModule to imports
+  imports: [ CommonModule, RouterModule, MatSidenavModule, TitleCasePipe, MatIconModule],
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
 
-  // Define your menu items here
-  menuItems = [
-    { name: 'Home', icon: 'home', route: '/home' },
-    { name: 'About', icon: 'info', route: '/test' },
-    { name: 'Contact', icon: 'mail', route: '/data-display' }
+  isExpanded = true;
+  //sideMenuList: string[] = ['home', 'test', 'data-display'];
+  sideMenuList = [
+    { name: 'Home', icon: 'home', route: 'home' },
+    { name: 'About', icon: 'info', route: 'test' },
+    { name: 'Contact', icon: 'mail', route: 'data-display' }
   ];
 
+  toggleSidenav() {
+    console.log(this.isExpanded)
+    this.isExpanded = !this.isExpanded;
+  }
 }
